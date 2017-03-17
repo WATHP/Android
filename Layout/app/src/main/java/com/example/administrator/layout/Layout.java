793de477c01b397d6a369a1a587eb51e28070876
WATHP <594312342@qq.com>
2017-03-18 00:45:51 +0800

    .\Layout\

---
+++ Layout/app/src/main/java/com/example/administrator/layout/Layout.java
@@ -0,0 +1,13 @@
+package com.example.administrator.layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.table_layout);
+    }
+}
