6d723c298c3952f4637cd4c2d74d5a848772e50a
WATHP <594312342@qq.com>
2017-05-09 20:35:47 +0800

    .\intent_broacast\

---
+++ intent_broacast/Intent/IntentReceiver/app/src/main/java/com/example/administrator/intentreceiver/OtherActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.intentreceiver;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class OtherActivity extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		//从Intent当中根据key取得value
+		String value = intent.getStringExtra("testIntent");
+		Log.e("IntentReceiver-->Test", value);
+	}
+	
+}
